69035c769cc0ef5c68358c89bd9533af45728ce9
Abrar <abrarulhoque@gmail.com>
2025-09-05 19:35:45 +0600

    fixed css style for add to cart

--- slidefirePro-widgets.php
+++ slidefirePro-widgets.php
@@ -3,7 +3,7 @@
  * Plugin Name:       SlideFirePro Widgets
  * Plugin URI:        https://abrarulhoque.com/
  * Description:       A collection of custom Elementor widgets for SlideFirePro, including WooCommerce category filters and product grids.
- * Version:           1.30.0
+ * Version:           1.31.0
  * Requires at least: 6.5
  * Requires PHP:      8.1
  * Author:            Abrar
